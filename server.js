93b295b5ea47909ecbc893ac99d8b9092d9051cc
MahdiSelmani <92759993+MahdiSelmani@users.noreply.github.com>
2022-01-17 12:36:46 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ const middleware = (req, res, next) => {
     var date = new Date();
     var day = date.getDay()
     var hour = date.getHours()
-    if (day !== 6 || day !== 7) res.send('<h1>Services are not avaible weekends. Please come Monday to Friday, 8 AM to 5 PM. </h1>')
+    if (day == 6 || day == 7) res.send('<h1>Services are not avaible weekends. Please come Monday to Friday, 8 AM to 5 PM. </h1>')
     else if (hour<9 || hour > 17) res.send('<h1>Services are not avaible weekends. Please come Monday to Friday, 8 AM to 5 PM. </h1>')
-    else next(); 
+    next(); 
 }
 app.use(middleware)
 
